347aa7d4de1a85af8c303cff72da4c594b515e28
dhafiskndr <19240312@bsi.ac.id>
2026-06-15 18:49:26 +0700

    fix: update Flutter environment variables for correct paths

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\PadiGuard_Karawang"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\PadiGuard_Karawang\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\Android\flutter_windows_3.3.9-stable"
+export "FLUTTER_APPLICATION_PATH=D:\Project Laravel\PadiGuard_Karawang"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Project Laravel\PadiGuard_Karawang\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
